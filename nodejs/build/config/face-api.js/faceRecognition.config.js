434e0aca62564a1c4f1dec4c655de45d1275b345
tvconss <tranconcoder@gmail.com>
2024-10-24 09:34:08 +0700

    remove (build): Remove nodejs build folder

--- nodejs/build/config/face-api.js/faceRecognition.config.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MIN_FACE_UPLOAD = exports.MIN_CONFIDENCE = void 0;
-exports.MIN_CONFIDENCE = 0.3;
-exports.MIN_FACE_UPLOAD = 3;
